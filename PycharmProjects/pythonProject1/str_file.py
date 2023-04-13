2f62a34363e0f4e8d6f39583d974472da5e5eb0b
ywaysi2004 <igritsenko36586@gmail.com>
2023-04-13 11:59:03 +0300

    CommitH

---
+++ PycharmProjects/pythonProject1/str_file.py
@@ -0,0 +1,15 @@
+value = "Mark, Kyiv, Yurii, Lviv, Serafim, Taras, Kherson"
+value_list = value.split(',')
+print(value_list)
+name_0 = value_list[0]
+name_1 = value_list[2]
+name_2 = value_list[4]
+name_3 = value_list[5]
+city_0 = value_list[1]
+city_1 = value_list[3]
+city_2 = value_list[6]
+
+
+print(name_0, name_1, name_2, name_3)
+print(city_0, city_1, city_2)
+
