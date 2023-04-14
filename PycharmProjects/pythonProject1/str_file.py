8545cf1838eeb1ed43c4a5817d96f9718f5a1c43
ywaysi2004 <igritsenko36586@gmail.com>
2023-04-14 12:41:46 +0300

    Second Commit

--- PycharmProjects/pythonProject1/str_file.py
+++ PycharmProjects/pythonProject1/str_file.py
@@ -13,3 +13,14 @@ city_2 = value_list[6]
 print(name_0, name_1, name_2, name_3)
 print(city_0, city_1, city_2)
 
+
+new_list = ['Mark', 'Kherson', 'Yurii', 'Serafim', 'Taras']
+
+new_list.insert(0, "Allen, " "Joe")
+print(new_list)
+new_list.append("Ukraine, " "Germany")
+print(new_list)
+
+
+
+
